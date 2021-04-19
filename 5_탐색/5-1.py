5facfd219509a516d4c66dfac061e439552d48b1
hk <khman119@gmail.com>
2021-04-19 10:58:17 +0900

    스택 예제

---
+++ 5_탐색/5-1.py
@@ -0,0 +1,13 @@
+stack = []
+
+#삽입5 - 삽입2 - 삽입3 - 삽입7 - 삭제 - 삽입1 - 삽입4 - 삭제
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack)
