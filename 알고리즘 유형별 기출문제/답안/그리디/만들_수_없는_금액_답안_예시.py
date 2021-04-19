3eb647b30452604c82b8c3b07de4bf10d7cb0394
hk <khman119@gmail.com>
2021-04-20 01:29:48 +0900

    '만들 수 없는 금액' 풀이

---
+++ 알고리즘 유형별 기출문제/답안/그리디/만들_수_없는_금액_답안_예시.py
@@ -0,0 +1,15 @@
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+target = 1
+
+for x in data:
+    if target < x:
+        break
+    target += x
+
+print(target)
+
+# 5
+# 3 2 1 1 9
