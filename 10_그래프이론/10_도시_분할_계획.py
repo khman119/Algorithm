36042d69dec3162cab54b62cdf897d73feafdeed
hk <khman119@gmail.com>
2021-04-19 11:32:21 +0900

    '도시 분할 계획' 문제 답안

---
+++ 10_그래프이론/10_도시_분할_계획.py
@@ -0,0 +1,52 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+n, m = map(int, input().split())
+parent = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    parent[i] = i
+
+edges = []
+result = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+
+edges.sort()
+last = 0
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+        last = cost
+
+print(result - last)
+
+
+# 7 12
+# 1 2 3
+# 1 3 2
+# 3 2 1
+# 2 5 2
+# 3 4 4
+# 7 3 6
+# 5 1 5
+# 1 6 2
+# 6 4 1
+# 6 5 3
+# 4 5 3
+# 6 7 4
