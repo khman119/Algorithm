d67f3f28a9f8920d453995aa42947b10c63e7619
hk <khman119@gmail.com>
2021-05-28 01:17:37 +0900

    백준 1003_피보나치_함수 solved

---
+++ 알고리즘 유형별 기출문제/백준/다이나믹프로그래밍/1003_피보나치_함수.py
@@ -0,0 +1,15 @@
+tc = int(input())
+
+for _ in range(tc):
+    n = int(input())
+    dp0 = [0] * (n + 1)
+    dp1 = [0] * (n + 1)
+    if n == 0:
+        dp0[0] = 1
+    if n >= 1:
+        dp0[0] = 1
+        dp1[1] = 1
+    for i in range(2, n + 1):
+        dp0[i] = dp0[i - 1] + dp0[i - 2]
+        dp1[i] = dp1[i - 1] + dp1[i - 2]
+    print(dp0[n], dp1[n])
