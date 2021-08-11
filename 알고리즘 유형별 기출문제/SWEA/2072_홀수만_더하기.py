1128f5f4f7437653707c071464a8fe70dbab991f
khman119 <khman119@gmail.com>
2021-08-11 21:30:21 +0900

    Apply .gitignore

--- 알고리즘 유형별 기출문제/SWEA/2072_홀수만_더하기.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-for test_case in range(1, T + 1):
-    result = 0
-    arr = list(map(int, input().split()))
-    n = len(arr)
-    for i in range(n):
-        if arr[i] % 2 == 1:
-            result += arr[i]
-    print("#" + str(test_case) + " " + str(result))
