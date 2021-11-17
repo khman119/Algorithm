02401fc6fcb9db91b1b0ac94cb0e18729230c6df
hk <khman119@gmail.com>
2021-11-17 21:34:11 +0900

    BOJ_15650_N과_M(2)_solved

---
+++ 알고리즘 유형별 기출문제/백준/백트래킹/15650_N과_M(2).py
@@ -0,0 +1,7 @@
+from itertools import combinations
+n, m = map(int, input().split())
+arr = [i for i in range(1, n + 1)]
+for x in combinations(arr, m):
+    for y in x:
+        print(y, end=' ')
+    print()
